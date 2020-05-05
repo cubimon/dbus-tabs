763647246abb10c63e4da48b1f5ac6a67359eacf
cubimon <cubimon93@gmail.com>
2020-05-05 21:45:11 +0200

    ignore files

---
+++ web-ext-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ignoreFiles: [
+    'dbus.py',
+    'dbus_tabs.json',
+    'install_native.sh',
+    'package.json',
+    'yarn.lock',
+    'README.md',
+    'web-ext-config.js'
+  ]
+}
